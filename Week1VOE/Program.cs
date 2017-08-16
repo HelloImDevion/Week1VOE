896b52d981e0f231f29b584ab67a8a56b7d9d54c
HelloImDevion <devionfothergill@gmail.com>
2017-08-15 20:50:43 -0400

    Created more Types

--- Week1VOE/Program.cs
+++ Week1VOE/Program.cs
@@ -10,6 +10,54 @@ namespace Week1VOE
     {
         static void Main(string[] args)
         {
+            //string and int 
+            //string name = "Devion Fothergill";
+            //Console.WriteLine(name);
+
+            //name = "Dave";
+            //Console.WriteLine(name);
+            //string Name = "Devion Fothergill";
+
+            //int birthMonth = 10;
+            //int age = 19;
+            //int population = 100000000;
+            //Console.WriteLine(birthMonth);
+
+            //Float Variable types 
+            //float number = 2.164403304897294383961032f;
+            //// 6 numbers after decimal
+            //double eightNumber = 2.164403304897294383961032d;
+            //// 14 numbers after decimal
+            //decimal partialNumber = 2.164403304897294383961032m;
+            //// exact numbers 
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(eightNumber);
+            //Console.WriteLine(partialNumber);
+
+            //boolean
+            //bool isPresident = true;
+            //bool seatTaken = false;
+
+            //// Char or Character
+            //Char lastLetter = 'j';
+
+            //Jessica is 23 years old.
+            //Sam is 47.
+            //Jessica is younger than Sam.
+
+            //int jessicaAge = 23;
+            //int samAge = 47;
+
+            //Console.WriteLine(jessicaAge < samAge);
+
+            int mikeBill = 13;
+            int friendBill = 13;
+            int friend2Bill = 13;
+
+            Console.WriteLine(mikeBill + friendBill + friend2Bill );
+
+
         }
     }
 }
